24afc3be233e01648f5aed89f18c371b7aac71b9
nguyencongminh <minh123minh123minh123@gmail.com>
2022-12-13 09:29:16 +0700

    r
    r

--- config/db.php
+++ config/db.php
@@ -10,9 +10,14 @@
 
     private function __construct()
     {
-        $this->conn = new PDO("mysql:host={$this->host};
-        dbname={$this->name}", $this->user,$this->pass,
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        try {
+			$this->conn = new PDO("mysql:host={$this->host};
+			dbname={$this->name}", $this->user,$this->pass,
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		} catch(PDOException $e)
+		{
+		  echo $e->getMessage();                         
+		}
     }
 
     public static function getInstance()
